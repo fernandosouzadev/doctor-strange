cbc374c5fdc5ea3cca7a9353199e90f8841b38c8
Fernando <fernandosouzadev@gmail.com>
2022-01-30 16:06:19 -0300

    remove propety to iframe

--- pages/index.tsx
+++ pages/index.tsx
@@ -80,7 +80,7 @@ export default function Home() {
             }
             >
             <button onClick={handleCloseModal} className={styles.closemodal}>{<MdOutlineClose/>}</button>
-            <iframe width="600" height="400" src="https://www.youtube.com/embed/Rt_UqUm38BI?controls=0" frameborder="0" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+            <iframe width="600" height="400" src="https://www.youtube.com/embed/Rt_UqUm38BI?controls=0" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
             </Modal>
           </div>
           </motion.main>
