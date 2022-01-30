72b93a02c7f90b302172cc5edf41e7edd976f53e
Fernando <fernandosouzadev@gmail.com>
2022-01-30 15:57:50 -0300

    Init project Doctor Strange

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,30 @@
+import { ImFacebook2 } from 'react-icons/im';
+import { BsInstagram } from 'react-icons/bs';
+import { FaTwitterSquare } from 'react-icons/fa';
+import styles  from './styles.module.scss'
+
+
+export function Header(){
+    return (
+        <div className={styles.header}>
+            <div className={styles.logo}>
+                <img src="/images/logo.png" alt="Logo Marvel Studios" />
+            </div>
+            <div className={styles.menu}>
+                <ul>
+                    <li><a href="#" className={styles.active}>HOME</a></li>
+                    <li><a href="#">SINOPSE</a></li>
+                    <li><a href="#">CHARACTERS</a></li>
+                    <li><a href="#">ABOUT</a></li>
+                </ul>
+                <div className={styles.social}>
+                    <a href=""><ImFacebook2/></a>
+                    <a href=""><BsInstagram/></a>
+                    <a href=""><FaTwitterSquare/></a>
+                </div>
+            </div>
+            
+
+        </div>
+    );
+}
