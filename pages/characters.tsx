a190f9f6dc1a4ba868492b6d9171a98280aefe68
Fernando <fernandosouzadev@gmail.com>
2022-02-13 19:30:22 -0300

    including default value for text area

--- pages/characters.tsx
+++ pages/characters.tsx
@@ -60,7 +60,7 @@ export default function Characters(props:Character) {
                 }
 
                   <div>
-                      <textarea>Wanda Maximoff, also known as the Scarlet Witch, is a native of Sokovia who grew up with her fraternal 
+                      <textarea defaultValue='Wanda Maximoff, also known as the Scarlet Witch, is a native of Sokovia who grew up with her fraternal 
                       twin brother, Pietro. Born with the latent mythical ability to harness Chaos Magic, she developed a hatred against Tony Stark and rallied 
                       anti-American protests after the Novi Grad Bombings killed her parents. Years later, in an effort to help purge their country of strife, the 
                       twins joined HYDRA and agreed to undergo experiments with the Scepter under the supervision of Wolfgang von Strucker, with the Mind Stone awakening
@@ -68,7 +68,7 @@ export default function Characters(props:Character) {
                       Ultron to get their revenge on Stark, but abandoned him when they discovered his true intentions to eradicate humanity. Wanda and Pietro joined
                       the Avengers during the Battle of Sokovia to stop Ultron; Pietro was killed during the battle but Wanda survived, and shortly after relocated 
                       to the Avengers Compound in the United States of America.
-                      During the Avengers Civil War, she sided with Captain America and was briefly  imprisoned on the Raft before Rogers freed her alongside</textarea></div>
+                      During the Avengers Civil War, she sided with Captain America and was briefly  imprisoned on the Raft before Rogers freed her alongside'></textarea></div>
                   </div>
               </div>
               <AnimatePresence>
