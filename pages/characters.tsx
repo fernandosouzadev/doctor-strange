4e810cae442468d3f99d30f631814e9cf0c1df1e
Fernando <fernandosouzadev@gmail.com>
2022-02-14 07:48:42 -0300

    Add tabs with material ui

--- pages/characters.tsx
+++ pages/characters.tsx
@@ -28,7 +28,7 @@ type CharacterState = {
 
 }
 
-function TabPanel(props) {
+function TabPanel(props: { [x: string]: any; children: any; value: any; index: any; }) {
   const { children, value, index, ...other } = props;
 
   return (
@@ -54,7 +54,7 @@ TabPanel.propTypes = {
   value: PropTypes.number.isRequired,
 };
 
-function a11yProps(index) {
+function a11yProps(index: number) {
   return {
     id: `simple-tab-${index}`,
     'aria-controls': `simple-tabpanel-${index}`,
@@ -64,7 +64,7 @@ function a11yProps(index) {
 export default function Characters(props:Character) {
   const [value, setValue] = React.useState(0);
 
-  const handleChange = (event, newValue) => {
+  const handleChange = (event: any, newValue: React.SetStateAction<number>) => {
     setValue(newValue);
   };
   const characters = props.dados
