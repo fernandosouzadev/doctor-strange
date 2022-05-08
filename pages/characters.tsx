483ce83f8ec5d6f1c582c0a74c5561b6093d10df
Fernando <fernandosouzadev@gmail.com>
2022-05-08 19:23:59 -0300

    Finalization Project, correction erro API

--- pages/characters.tsx
+++ pages/characters.tsx
@@ -198,7 +198,7 @@ export default function Characters(props:Character) {
                                 <h2>{comic?.title}</h2>
                                 </a>
                               </div>
-                            ) : <h1>We couldn&apos;t find any comics for this character!</h1>  
+                            ) : <h1>We couldn't find any comics for this character!</h1>  
                           } 
                         </div>
                       </TabPanel>
@@ -223,7 +223,7 @@ export default function Characters(props:Character) {
 
 export async function getStaticProps() {
   // Fetch data from external API
-  const res = await fetch(`https://doctor-strange.vercel.app//api/characters`)
+  const res = await fetch(`https://doctor-strange.vercel.app/api/characters`)
   const dados = await res.json()
   // Pass data to the page via props
   return { 
