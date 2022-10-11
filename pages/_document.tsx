bec5f0ac61b30498ff0ad1fc405dae5c95ccbfca
Fernando Souza <fernandosouzadev@gmail.com>
2022-10-11 13:52:57 -0300

    rst

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,17 +6,17 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+        <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "e1g3pnzrpy");
+</script>
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
             <link rel="preconnect" href="https://fonts.gstatic.com"/>
             <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;700&display=swap" rel="stylesheet" />
-            <link rel="shortcut icon" href="/images/favicon.png" type="image/png" />  
-            <script type="text/javascript">
-                (function(c,l,a,r,i,t,y){
-                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                })(window, document, "clarity", "script", "e1g3pnzrpy");
-            </script> 
+            <link rel="shortcut icon" href="/images/favicon.png" type="image/png" /> 
         </Head>
         <body>
           <Main />
