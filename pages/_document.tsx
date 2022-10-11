b6141ebb9cafc71c4ecfd19f2a04d2c7a9cc5635
Fernando Souza <fernandosouzadev@gmail.com>
2022-10-11 13:59:47 -0300

    teste

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,13 +8,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        <Script type="text/javascript">
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "e1g3pnzrpy");
-</Script>
+        <script type="text/javascript" src="/static/script.js"></script>
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
             <link rel="preconnect" href="https://fonts.gstatic.com"/>
             <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;700&display=swap" rel="stylesheet" />
