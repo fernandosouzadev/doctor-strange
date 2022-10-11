a79be785e5c5fa88d44251843c4a81ef231906a0
Fernando Souza <fernandosouzadev@gmail.com>
2022-10-11 13:55:07 -0300

    kkkis

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
+
 
 class MyDocument extends Document {
 
@@ -6,13 +8,13 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        <script type="text/javascript">
+        <Script type="text/javascript">
     (function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
     })(window, document, "clarity", "script", "e1g3pnzrpy");
-</script>
+</Script>
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
             <link rel="preconnect" href="https://fonts.gstatic.com"/>
             <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;700&display=swap" rel="stylesheet" />
